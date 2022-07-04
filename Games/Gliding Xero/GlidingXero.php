5933212c4e6e0b51f446dd8d39ac375132ad925c
Chimerasite <cheyennebolders@gmail.com>
2022-07-04 11:17:18 +0200

    Start Gliding Xero game

---
+++ Games/Gliding Xero/GlidingXero.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link href="GlidingXero.css" rel="stylesheet" type="text/css">
+<script src="GlidingXero.js"></script>
+</head>
+<body onload="startGame()">
+
+</body>
+</html>
